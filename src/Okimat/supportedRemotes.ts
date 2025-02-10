f8a833623c4657d85fe15fcd442f35a4ba354797
Richard Hopton <richard@thehoptons.com>
2025-02-09 23:02:12 -0800

    (Okimat) Add 82418, 93329 & 93332 remote codes

--- src/Okimat/supportedRemotes.ts
+++ src/Okimat/supportedRemotes.ts
@@ -57,6 +57,24 @@ export const supportedRemotes: Dictionary<Remote> = {
       UBL: 0x20000,
     },
   },
+  '82418': {
+    name: 'RF TOPLINE',
+    commands: {
+      Flat: 0x000000aa,
+      BackDown: 0x2,
+      BackUp: 0x1,
+      LegsDown: 0x8,
+      LegsUp: 0x4,
+      Memory1: 0x1000,
+      Memory2: 0x2000,
+      MemorySave: {
+        data: 0x10000,
+        count: 10,
+        waitTime: 200,
+      },
+      UBL: 0x20000,
+    },
+  },
   '91244': {
     name: 'RF-FLASHLINE',
     commands: {
@@ -66,6 +84,58 @@ export const supportedRemotes: Dictionary<Remote> = {
       LegsDown: 0x8,
       LegsUp: 0x4,
       UBL: 0x20000,
+    },
+  },
+  '93329': {
+    name: 'RF TOPLINE',
+    commands: {
+      Flat: 0x0000002a,
+      HeadDown: 0x20,
+      HeadUp: 0x10,
+      BackDown: 0x2,
+      BackUp: 0x1,
+      LegsDown: 0x8,
+      LegsUp: 0x4,
+      Memory1: 0x1000,
+      Memory2: 0x2000,
+      Memory3: 0x4000,
+      Memory4: 0x8000,
+      MemorySave: {
+        data: 0x10000,
+        count: 10,
+        waitTime: 200,
+      },
+      UBL: {
+        data: 0x20000,
+        count: 50,
+        waitTime: 100,
+      },
+    },
+  },
+  '93332': {
+    name: 'RF TOPLINE',
+    commands: {
+      Flat: 0x000000aa,
+      HeadDown: 0x20,
+      HeadUp: 0x10,
+      BackDown: 0x2,
+      BackUp: 0x1,
+      LegsDown: 0x8,
+      LegsUp: 0x4,
+      FeetDown: 0x20,
+      FeetUp: 0x40,
+      Memory1: 0x1000,
+      Memory2: 0x2000,
+      MemorySave: {
+        data: 0x10000,
+        count: 10,
+        waitTime: 200,
+      },
+      UBL: {
+        data: 0x20000,
+        count: 50,
+        waitTime: 100,
+      },
     },
   },
 };
